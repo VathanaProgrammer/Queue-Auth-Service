07f707519ab9c883e29564ea8d07ae6e2e05e99f
Vathana <siengvathana1@gmail.com>
2026-05-05 17:12:49 +0700

    Update disable db

--- src/main/java/com/example/auth/controller/HomeController.java
+++ src/main/java/com/example/auth/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.example.auth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/")
-    public String hello() {
-        return "queue-auth-service is running on port ${server.port}...";
+    public String home() {
+        return "queue-auth-service is running on port " + serverPort + "...";
     }
 }
